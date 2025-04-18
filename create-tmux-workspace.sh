e90cf2bd3541082e305b8747f5f4d6b057e599d4
Leonard <leonard.ruebel@gmail.com>
2025-04-18 11:51:14 +0200

    tmux create flags

--- create-tmux-workspace.sh
+++ create-tmux-workspace.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
 
-DIR_NAME=${PWD##*/}
-INPUT=$1
+path=""
+name=""
+detached=false
 
-if [ -n "$INPUT" ]; then
-    SESSION_NAME=$INPUT
+#https://flokoe.github.io/bash-hackers-wiki/howto/getopts_tutorial
+while getopts :n:p:d opt; do
+    case $opt in
+    n)
+        name=$OPTARG
+        ;;
+    p)
+        path=$OPTARG
+        ;;
+    d)
+        detached=true
+        ;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+
+WS_PATH=$1
+if [ ! -z "$path" ] && [ -d $path]; then
+    WS_PATH=$path
+elif [ -z "${WS_PATH}" ]; then
+    WS_PATH=$PWD
+fi
+
+if [ ! -d $WS_PATH ]; then
+    echo 'dir does not exist' 1>&2
+    exit 1
+fi
+
+DIR_NAME=${WS_PATH##*/}
+
+if [ ! -z "${name}" ]; then
+    SESSION_NAME=$name
 else
     SESSION_NAME=$DIR_NAME
 fi
 
-if ! tmux new-session -d -s $SESSION_NAME; then
+if [ ! -z "${path}" ] && [ -d $path ]; then
+    echo valid dir $path
+fi
+
+if tmux has-session -t $SESSION_NAME &>/dev/null; then
+    if ! $detached; then
+        tmux attach -t $SESSION_NAME
+    fi
+    exit
+fi
+
+if ! tmux new-session -d -s $SESSION_NAME -c $WS_PATH; then
     exit
 fi
 
@@ -18,10 +60,14 @@ FG_COLOR=$($HOME/dotfiles/color-picker/color-picker $SESSION_NAME | sed -n '2 p'
 tmux set -t $SESSION_NAME status-bg $BG_COLOR
 tmux set -t $SESSION_NAME status-fg $FG_COLOR
 
-tmux rename-window -t 1 "nvim"
-tmux send-keys -t "nvim" "nvim ." C-m
+tmux rename-window -t $SESSION_NAME:1 "nvim"
+tmux send-keys -t $SESSION_NAME:1 "nvim ." C-m
 
 tmux new-window -t $SESSION_NAME:2 -n 'zsh'
+
+if $detached; then
+    exit
+fi
 
 sleep 0.6
 
