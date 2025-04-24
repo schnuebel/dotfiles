4cc28ae9204b4e0a355697a89c13ada01d75034f
leo <leonard.ruebel@gmail.com>
2025-04-24 17:23:38 +0200

    install

--- install.sh
+++ install.sh
@@ -32,6 +32,9 @@ rm go1.24.2.linux-amd64.tar.gz
 #######################################
 #node, nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 nvm install node
 nvm use node
 
